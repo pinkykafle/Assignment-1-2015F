fa76bf2716c6f1c3e2ea5f43bf34a528ba5e39b9
Pinky Kafle <c0664982@mylambton.ca>
2015-09-14 13:54:20 -0400

    Hello World

--- src/assignment/pkg1/Assignment1.java
+++ src/assignment/pkg1/Assignment1.java
@@ -16,6 +16,9 @@ public class Assignment1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    
+        System.out.println("Hello World!");
     }
+    
     
 }
