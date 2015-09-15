cfe15d06194a3523bccbed317ffc3e35e6ab327a
Pinky Kafle <c0664982@mylambton.ca>
2015-09-15 15:43:44 -0400

    Assignment 1

--- src/assignment/pkg1/Assignment1.java
+++ src/assignment/pkg1/Assignment1.java
@@ -18,6 +18,34 @@ public class Assignment1 {
         // TODO code application logic here
     
         System.out.println("Hello World!");
+        
+        /********************************
+         * Assignment #1
+         * 
+         * Job Posting
+         * Back End Java Developer 
+         * Tundra Technical Solution - Toronto, ON
+         * http://www.indeed.ca/cmp/Tundra-Technical-Solution/jobs/Back-End-Java-Developer-be6b886816880859?q=java
+         * 
+         * Skills:-
+         * 
+         * Java EE - Java Platform, Enterprise Edition - https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+         * Spring Framework - Application Framework - https://en.wikipedia.org/wiki/Spring_Framework
+         * Hadoop - Open-source software Framework - https://en.wikipedia.org/wiki/apache_hadoop
+         * HBASE - an open-source - http://en.wikipedia.org/wiki/apache_hbase
+         * PIG - high-level platform for MapReduce program - http://en.wikipedia.org/wiki/pig_(programming_tool)
+         * SQL database - Structured Query Language - https://en.wikipedia.org/wiki/SQL
+         * NoSQL database - non SQL - https://en.wikipedia.org/wiki/NoSQL
+         * Linux - operating system - https://en.wikipedia.org/wiki/Linux
+         * 
+         * Salary:-
+         * 
+         * Toronto - 
+         * Vancouver - 
+         * Waterloo - 
+         */
+         
+         
     }
     
     
